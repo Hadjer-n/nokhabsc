922e0658276332379fd651d182748ae3a0701fa3
Hadjer Nial <n.hadjer@etu.univ-mascara.dz>
2025-11-28 21:07:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Portfolio from './Portfolio';
-import ma from './mapworld';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
